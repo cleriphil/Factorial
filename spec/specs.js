93337e504e2102f4faaa123b75599bc82cb5ab51
Cory Spitzer & cleri philiastides <student@epicodus.com>
2015-05-26 12:18:07 -0700

    determine the factorial of 2, 0 and 1

---
+++ spec/specs.js
@@ -0,0 +1,13 @@
+describe("factorial", function() {
+  it("returns 1 as the factorial of 0", function() {
+    expect(factorial(0)).to.equal(1);
+  });
+
+  it("returns 1 as the factorial of 1", function() {
+    expect(factorial(1)).to.equal(1);
+  });
+
+  it("returns 2 as the factorial of 2", function() {
+    expect(factorial(2)).to.equal(2);
+  });
+});
